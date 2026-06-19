4f4a90a6c974d105f5580b071bda23beb92df75a
sajid-shaikh-77 <mrsid966@gmail.com>
2026-06-19 12:54:21 +0530

    Implement custom login route in admin.routes.js with success response for testing

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,7 +4,14 @@ import { loginAdmin, registerAdmin } from "../controllers/admin.controller.js";
 const router = Router();
 
 router.route("/register").post(registerAdmin)
-router.route("/login").post(loginAdmin)
+// router.route("/login").post(loginAdmin)
 
+router.post("/login", (req, res) => {
+    console.log("LOGIN HIT");
+    res.status(200).json({
+        success: true,
+        message: "Route reached"
+    });
+});
 
 export default router
